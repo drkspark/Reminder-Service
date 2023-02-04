5473d22ae896c9da31145b791daf2d9ba8e3b6f2
drkspark <madhusudan753@gmail.com>
2023-02-04 10:56:31 +0530

    Basic Setup with Modules

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { PORT } = require("./config/serverConfig");
+
+const app = express();
+
+const startAndStartServer = () => {
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.listen(PORT, () => {
+        console.log(`Server started on Port ${PORT}`);
+    });
+};
+
+startAndStartServer();
