11af4d65a9416d6b00ac051e69c607364a46e3dd
drkspark <madhusudan753@gmail.com>
2023-03-15 10:05:31 +0530

    A bit of Refactoring

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { PORT } = require("./config/serverConfig");
-const {sendBasicEmail} = require('./services/email-service');
+const { sendBasicEmail } = require("./services/email-service");
 const jobs = require("./utils/jobs");
-const TicketController = require('./controllers/ticker-controler');
-
-
-
+const TicketController = require("./controllers/ticket-controler");
 
 const startAndStartServer = () => {
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
 
-    app.post('/api/v1/tickets', TicketController.create);
+    app.post("/api/v1/tickets", TicketController.create);
 
     app.listen(PORT, () => {
         console.log(`Server started on Port ${PORT}`);
